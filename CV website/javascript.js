0b4080b8fc55604b151de13b1c04e733e5313a18
oskarsiljins123 <oskarsiljins123@gmail.com>
2026-08-05 05:38:23 +0100

    Add files via upload

---
+++ CV website/javascript.js
@@ -0,0 +1,43 @@
+const navTitle = document.getElementById("page_title");
+
+window.addEventListener("scroll", () => {
+        const scrollY = window.scrollY;
+
+        if (scrollY < 500) {
+            navTitle.textContent = "Home";
+        } else if (scrollY < 1000) {
+            navTitle.textContent = "Brief Description";
+        } else if (scrollY < 1500) {
+            navTitle.textContent = "Education";
+        } else if (scrollY < 2000) {
+            navTitle.textContent = "Work Experience";
+        }
+          else if (scrollY < 2500) {
+            navTitle.textContent = "Skills";
+        }
+        else if (scrollY < 3000) {
+            navTitle.textContent = "Projects";
+        }
+    });
+const button = document.querySelector(".btn");
+const menu = document.querySelector(".menu");
+const sections = document.querySelectorAll("section");
+
+button.addEventListener("click", () => {
+    menu.classList.toggle("active");
+
+    if (menu.classList.contains("active")) {
+        button.textContent = "✕";
+
+        sections.forEach(section => {
+            section.style.paddingRight = "25%";
+        });
+
+    } else {
+        button.textContent = "☰";
+
+        sections.forEach(section => {
+            section.style.paddingRight = "0";
+        });
+    }
+});
